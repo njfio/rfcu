9c7663e9883a92d724bad166b031b960e43389f3
njfio <7220+njfio@users.noreply.github.com>
2024-06-06 21:45:31 -0400

    Added Fibonacci sequence calculation functionality

--- fibonacci.rb
+++ fibonacci.rb
@@ -25,6 +25,31 @@ def count_primes(arr)
   arr.count { |num| prime?(num) }
 end
 
+# Function to graph the Fibonacci sequence
+def graph_fibonacci(sequence)
+  require 'matplotlib'
+  x = (0...sequence.length).to_a
+  y = sequence
+  Matplotlib.plot(x, y)
+  Matplotlib.xlabel('Index')
+  Matplotlib.ylabel('Fibonacci Number')
+  Matplotlib.title('Fibonacci Sequence')
+  Matplotlib.show
+end
+
+# Function to graph the primes in the Fibonacci sequence
+def graph_primes(sequence)
+  require 'matplotlib'
+  primes = sequence.select { |num| prime?(num) }
+  x = sequence.map.with_index { |num, i| i if prime?(num) }.compact
+  y = primes
+  Matplotlib.scatter(x, y)
+  Matplotlib.xlabel('Index')
+  Matplotlib.ylabel('Prime Number')
+  Matplotlib.title('Primes in Fibonacci Sequence')
+  Matplotlib.show
+end
+
 # Main function to run the script
 def main
   puts "Enter a number:"
@@ -45,6 +70,10 @@ end
 
 # Run the main function
 main
+
+
+
+
 
 
 # Function to graph the Fibonacci sequence
